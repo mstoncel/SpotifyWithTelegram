02a3cd7bdba1ed76e7d76ba4e81f8347ad0251e9
Mesut Öncel <mesutoncel91@gmail.com>
2019-03-18 11:02:28 +0300

    add try-except for getFromDict method

--- helpers.py
+++ helpers.py
@@ -1,5 +1,18 @@
 from functools import reduce
+from typing import Union
 import operator
 
-def getFromDict(dataDict, mapList):
-    return reduce(operator.getitem, mapList, dataDict)
+def getFromDict(dataDict: dict, mapList:list)->Union[str,int]:
+    value = None
+    """
+        data = {'a': {'r': 1, 's': 2, 't': 3}
+        getFromDict(data,['a','s'])
+        output: 2
+
+    """
+    try:
+        value=reduce(operator.getitem, mapList, dataDict)
+    except KeyError:
+        pass
+    finally:
+        return value
